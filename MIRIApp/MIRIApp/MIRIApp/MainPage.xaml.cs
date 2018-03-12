8b31587a86116a0a0a1318474af230a9df8913f5
Richard Donaldson <bearofthesea@gmail.com>
2018-03-12 16:20:33 +0000

    Add Project files
    created and added project to Repo

---
+++ MIRIApp/MIRIApp/MIRIApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MIRIApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            
+		}
+
+        async void OnClick(object Sender, EventArgs e)
+        {
+            await this.DisplayAlert("Alert", "You have pressed a button", "Yes", "No");
+            Button button = (Button)Sender;
+            string buttonText = button.Text;
+            
+                switch(buttonText)
+            {
+                case "QR":
+                    await Navigation.PushAsync(new QRPage());
+                    break;
+                case "btn2":
+                    break;
+
+            }
+        }
+
+	}
+}
