1485fe1e4ae9ccf4d9741c223b42f6f659e9099e
Bran Flakes <branflakes@ymail.com>
2018-04-02 17:57:40 +0000

    + Add splash screen + Generate assorted splash logo place holders

---
+++ MIRIApp/MIRIApp/MIRIApp.Android/Splash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MIRIApp.Droid
+{
+    [Activity(Label = "Splash", Theme = "@style/splashTheme", MainLauncher = true, NoHistory = true)]
+    public class Splash : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
