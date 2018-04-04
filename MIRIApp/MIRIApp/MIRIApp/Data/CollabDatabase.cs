36c1a9f41e5fe369a52db07ea5eff96ef51a78b9
bearofthesea <bearofthesea@gmail.com>
2018-04-04 15:22:57 +0100

    Add Collaborator Model and Database interface
    Added Collaborator Model, Interface for database and code for database to allow each platform to access local database path

---
+++ MIRIApp/MIRIApp/MIRIApp/Data/CollabDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using MIRIApp.Model;
+
+
+namespace MIRIApp.Data
+{
+  public class CollabDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public CollabDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<Collaborator>().Wait();
+        }
+
+        public Task<List<Collaborator>> GetCollaboratorAsync()
+        {
+            return database.Table<Collaborator>().ToListAsync();
+        }
+
+        
+
+
+    }
+}
---
+++ MIRIApp/MIRIApp/MIRIApp/IFileHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MIRIApp
+{
+    public interface IFileHelper
+    {
+        string GetLocalFilePath(string fileName);
+    }
+}
