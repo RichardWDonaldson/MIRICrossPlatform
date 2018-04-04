5fbd55038d5e915c02a51212b026b555ae40d335
Bran Flakes <branflakes@ymail.com>
2018-04-04 15:25:03 +0000

    + Add rotation lock

--- MIRIApp/MIRIApp/MIRIApp.Android/MainActivity.cs
+++ MIRIApp/MIRIApp/MIRIApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MIRIApp.Droid
 {
-    [Activity(Label = "MIRIApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MIRIApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
