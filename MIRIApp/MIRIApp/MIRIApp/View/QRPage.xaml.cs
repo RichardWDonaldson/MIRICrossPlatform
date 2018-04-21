d5cef82dc84ba65b4234170bf90d83313e204552
bearofthesea <bearofthesea@gmail.com>
2018-04-21 18:08:29 +0100

    Fix QR Bug
    Fixed QR bug where pages would get stuck
    
    Co-Authored-By: Alex <alexbarker@users.noreply.github.com>

--- MIRIApp/MIRIApp/MIRIApp/View/QRPage.xaml.cs
+++ MIRIApp/MIRIApp/MIRIApp/View/QRPage.xaml.cs
@@ -22,12 +22,14 @@ namespace MIRIApp
         string[] split;
         bool state = false;
         public bool itemScanned = false;
+        
 
         public QRPage()
         {
             InitializeComponent();
             BindingContext = new ViewModel();
             
+
         }   
 
         protected override void OnAppearing()
@@ -38,7 +40,7 @@ namespace MIRIApp
             if(!state)
             {
                 QRCode.IsVisible = false;
-
+                QRButton.IsEnabled = false;
                 state = true;
                 ScanningPage();
                
@@ -47,7 +49,7 @@ namespace MIRIApp
             {
                 
                 QRCode.IsVisible = true;
-                
+                QRButton.IsEnabled = true;
                 //Navigation.PopAsync();
             }
          }
@@ -98,40 +100,41 @@ namespace MIRIApp
 
         private async void Button_OnClicked(object sender, EventArgs e)
         {
-            //List<Collaborator> list = await App.Database.GetCollaboratorAsync();
-            //var scannerPage = new ZXingScannerPage();
-            //await Navigation.PushAsync(scannerPage);
+            List<Collaborator> list = await App.Database.GetCollaboratorAsync();
+            var scannerPage = new ZXingScannerPage();
+            await Navigation.PushAsync(scannerPage);
 
-            //scannerPage.OnScanResult += (result) =>
-            //{
-            //    scannerPage.IsScanning = false;
-            //    Device.BeginInvokeOnMainThread(async () =>
-            //    {
-            //        await Navigation.PopAsync();
-            //        await DisplayAlert("Scanned Detail", result.Text, "OK");
-            //        char[] splitChar = { '/' };
-            //        split = result.Text.Split(splitChar);
-            //        Collaborator chosenCollab = FindCollaborator(list);
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                  //  await DisplayAlert("Scanned Detail", result.Text, "OK");
+                    char[] splitChar = { '/' };
+                    split = result.Text.Split(splitChar);
+                    Collaborator chosenCollab = FindCollaborator(list);
 
-            //        if(chosenCollab != null)
-            //        {
-            //            await Navigation.PushAsync(new ItemPage
-            //            {
-            //                BindingContext = chosenCollab as Collaborator
+                    if (chosenCollab != null)
+                    {
+                        await Navigation.PushAsync(new ItemPage
+                        {
+                            BindingContext = chosenCollab as Collaborator
 
-            //            });
+                        });
 
-            //        } else
-            //        {
-            //           await DisplayAlert("Invalid QR Code", "Invalid QR Code - Please try again", "OK");
-            //        }
-
-
-
+                    }
+                    else
+                    {
+                        await DisplayAlert("Invalid QR Code", "Invalid QR Code - Please try again", "OK");
+                    }
 
 
-               // });
-            //};
+
+
+
+                });
+            };
 
         }
 
